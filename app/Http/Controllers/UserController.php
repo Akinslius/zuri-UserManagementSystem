cc611ae1223316964e73f6c8ade927bbb14a54b6
Akinslius <akinyemi.akinwande@yahoo.com>
2022-07-16 15:39:16 +0100

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function CreateUser(){
+        //
+
+    }
+    public function EditUser(){
+        //
+
+    }
+    public function DeleteUser(){
+        //
+
+    }
+    public function GetUser(){
+        //
+
+    }
+
+
+
+}
