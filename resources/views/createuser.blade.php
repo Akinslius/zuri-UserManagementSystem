2ab9ead3c3db75050410036ea3195bfc3df8fcbb
Akinslius <akinyemi.akinwande@yahoo.com>
2022-07-17 20:50:38 +0100

    second commit

--- resources/views/createuser.blade.php
+++ resources/views/createuser.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit User page</title>
+    <title>Create User page</title>
 
 <!-- bootstrap css link -->  
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -21,26 +21,26 @@
             <!-- name -->
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" name="name" class="form-control" value="Edit your name" required>
+                <input type="text" name="name" class="form-control" placeholder="Enter your Name" required>
 
             </div>
             <!-- email-->
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="email" class="form-label">Email</label>
-                <input type="text" name="email" class="form-control" value="Edit your email" required>
+                <input type="text" name="email" class="form-control" placeholder="Enter your Email Address" required>
 
             </div>
 
             <!--phone-->
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="phone" class="form-label">Phone Number</label>
-                <input type="tel" name="email" class="form-control" value="Edit your email" required>
-                <input type="number" name="" id="">
-
-
+                <input type="tel" name="email" class="form-control" placeholder="Enter your Phone Number" required>
+        
             </div>
+            <input type="submit" value="Create User">
             
             </form>
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+
+namespace app\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/users',[UserController::class , 'GetUsers']);
+Route::get('/edituser',[UserController::class , 'EditUser']);
+Route::get('/createuser',[UserController::class , 'CreateUser']);
+Route::get('/deleteuser',[UserController::class , 'DeleteUser']);
